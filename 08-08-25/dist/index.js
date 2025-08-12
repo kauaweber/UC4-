208dbe8e00316fb88d16ce18f35b5425195bf770
kauaweber <wkauavictor@gmail.com>
2025-08-11 21:14:47 -0300

    Sua mensagem do commit aqui

---
+++ 08-08-25/dist/index.js
@@ -0,0 +1,24 @@
+"use strict";
+let readline = require("readline-sync");
+let controlador = true;
+while (controlador) {
+    console.log("=== MENU PRINCIPAL ===");
+    console.log("1 - Olá");
+    console.log("2 - Sobre");
+    console.log("3 - Sair");
+    const opcao = readline.question("Escolha uma opção: ");
+    switch (opcao) {
+        case "1":
+            console.log("Olá, seja bem-vindo!");
+            break;
+        case "2":
+            console.log("Este é um exemplo de menu com switch.");
+            break;
+        case "3":
+            console.log("Saindo do programa...");
+            controlador = false;
+            break;
+        default:
+            console.log("Opção inválida, por favor tente novamente.");
+    }
+}
